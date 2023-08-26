f9bd6142db714fda79cca18f5cde001b6910a791
maneldevs <maneldevs@gmail.com>
2023-08-26 09:44:35 +0000

    Configuration to develop on a two remote container, app and db

---
+++ src/main/java/es/maneldevs/demodockersimple/T1Repository.java
@@ -0,0 +1,7 @@
+package es.maneldevs.demodockersimple;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface T1Repository extends JpaRepository<T1, Long> {
+    
+}
--- src/main/java/es/maneldevs/demodockersimple/TestController.java
+++ src/main/java/es/maneldevs/demodockersimple/TestController.java
@@ -1,15 +1,27 @@
 package es.maneldevs.demodockersimple;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.RequiredArgsConstructor;
+
 @RestController
+@RequiredArgsConstructor
 public class TestController {
+
+    private final T1Repository repo;
 
     @GetMapping
     String test() {
-        String test = "hello world";
+        String test = "hello world!";
         return test;
+    }
+
+    @GetMapping("test1")
+    List<T1> test1() {
+        return repo.findAll();
     }
     
 }
